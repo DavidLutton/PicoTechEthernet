d5ae3a250a44bec851a923a3c9a9c9e74fe89c79
David Lutton <david@dalun.space>
2022-07-04 22:07:20 +0100

    Change data sink to influxdb

--- client.py
+++ client.py
@@ -1,10 +1,23 @@
 """."""
 
-import requests
 import socket
 from PicoTechEthernet import PicoTechEthernetCM3
 
-CM3 = PicoTechEthernetCM3(ip='192.168.57.200', port=6554)
+
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+token = "TOKEN"
+org = "ORG"
+bucket = "BUCKET"
+
+client = InfluxDBClient(url="http://127.0.0.1:8086", token=token)
+
+
+# sudo nmap -sU -p 6554 192.168.1.0/24
+CM3 = PicoTechEthernetCM3(ip='192.168.1.200', port=6554)
 
 while True:  # Loop forever
     try:
@@ -15,15 +28,18 @@ while True:  # Loop forever
         CM3.set('1w', b'Converting\x00')  # channel setup ??
 
         for load in next(CM3):
-            print(load)
+            # print(load)
 
             # Submit information to a time series database, InfluxDB
-            # eg  load = '0 value=0.21761050447821617'
+            # eg  load = '0, 0.21761050447821617'
             # AKA channel 0, 0.21761050447821617 mV
-            r = requests.post('http://localhost:8086/write?db=CM3', data=load)
 
-    except requests.exceptions.ConnectionError:
-        print('Connection Error to InfluxDB')
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+
+            data = f"Channel{load[0]} current={load[1]}"
+            print(data)
+            write_api.write(bucket, org, data)
+
     except socket.timeout:
         print('Connection timeout to PicoTech device')
 
